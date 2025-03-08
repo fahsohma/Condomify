06da8e2d99570324866bdee62067c736f6654824
Fabio Luiz Goncalves <fabio.flgs@hotmail.com>
2025-03-08 15:51:40 -0300

    Adiciona servidor básico com Express e MongoDB

---
+++ server.js
@@ -0,0 +1,29 @@
+// Importar as dependências
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Criar uma instância do Express
+const app = express();
+
+// Definir a porta do servidor
+const PORT = process.env.PORT || 5000;
+
+// Middleware para permitir requisições JSON e CORS
+app.use(cors());
+app.use(express.json());
+
+// Conectar ao MongoDB
+mongoose.connect('mongodb+srv://fahsohma:Teka.2014@cluster0.oyhsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => console.log('Conectado ao MongoDB!'))
+.catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
+
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('Servidor está funcionando!');
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
